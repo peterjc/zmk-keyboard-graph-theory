86b8ade4aef1c6b6ae088d1079f782a8b765668b
Peter J. A. Cock <p.j.a.cock@googlemail.com>
2026-04-27 17:55:09 +0100

    Draft GPIO allocation for Rugby Union (15-a-side) 30 key girth 10 design.
    This is to confirm the matrix and left/right split works in principle.
    Pin allocation rather ad hoc/random with considering the traces.
    
    [ci skip]

---
+++ boards/shields/rugby_union/roll-over.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+"""Analysis of 8-key roll-over in Rugby Union (15-a-side) keyboard.
+
+Graph logged as https://houseofgraphs.org/graphs/54569
+"""
+
+import numpy as np
+import networkx as nx
+from networkx.algorithms import bipartite
+from scipy import sparse
+
+rows = 15  # logical rather than physical, only r1 to r7 and r10 on RHS
+cols = 10  # logical rather than physical, only c2 to c3 on RHS
+layout_string = """
+            RC(0,0)  RC(0,1)  RC(8,2)  RC(8,9)  RC(14,7)         RC(2,2)  RC(1,3) RC(1,4) RC(2,5) RC(3,6)
+            RC(11,0) RC(12,1) RC(9,4)  RC(9,9)  RC(13,8)         RC(3,2)  RC(4,3) RC(6,4) RC(5,5) RC(4,6)
+            RC(14,0) RC(13,1) RC(11,6) RC(12,9) RC(10,5)         RC(10,8) RC(7,3) RC(5,7) RC(6,7) RC(7,8)
+"""  # from ZMK rugby-union-layouts.dtsi
+
+layout_keys = """
+                &kp Q  &kp W  &kp E  &kp R  &kp T       &kp Y &kp U &kp I     &kp O   &kp P
+                &kp A  &kp S  &kp D  &kp F  &kp G       &kp H &kp J &kp K     &kp L   &kp SEMI
+                &kp Z  &kp X  &kp C  &kp V  &kp B       &kp N &kp M &kp COMMA &kp DOT &kp SLASH
+"""  # from ZMK rugby-union.keymap
+pretty_chars = {
+    "DOT": ".",
+    "COMMA": ",",
+    "SLASH": "/",
+    "SEMI": ";",
+}  # see keymap_drawer.config.yaml
+thumbs = "BN"
+
+
+def parse_zmk_map(rc_layout, keys_layout, rows, cols):
+    # matrix = np.zeros((rows, cols), np.dtypes.StringDType)
+    matrix = np.zeros((rows, cols), str)  # single char!
+    keys = keys_layout.split()
+    for text in rc_layout.split():
+        assert text.startswith("RC(") and text.endswith(")") and text.count(",") == 1, (
+            text
+        )
+        action = keys.pop(0)
+        assert action == "&kp", action
+        k = keys.pop(0)
+        print(text[3:-1], k)
+        matrix[tuple(int(_) for _ in text[3:-1].split(","))] = pretty_chars.get(k, k)
+    return matrix
+
+
+pretty_matrix = parse_zmk_map(layout_string, layout_keys, rows, cols)
+bipartite_matrix = pretty_matrix.astype(np.bool)
+G = bipartite.from_biadjacency_matrix(sparse.csc_array(bipartite_matrix))
+
+import tempfile
+
+print("As a graph6 string:")
+with tempfile.NamedTemporaryFile(delete=False) as f:
+    nx.write_graph6(G, f.name, header=False)
+    _ = f.seek(0)
+    print(f.read().decode())
+
+print(bipartite_matrix.astype(int))
+print("Markdown matrix")
+print("|R/C|" + "|".join(f"{_:<3}" for _ in range(cols)) + "|")
+print("|---|" + "---|" * cols)
+print(
+    "|"
+    + " | \n|".join(
+        f"R{r:<2}| " + " | ".join(_ if _ else " " for _ in row)
+        for r, row in enumerate(pretty_matrix)
+    )
+    + " |"
+)
+
+print("Problem cycles of up to eight keys;")
+for cycle in nx.chordless_cycles(G, 10):
+    classes = [G.nodes[_]["bipartite"] for _ in cycle]
+    assert classes == [1, 0, 1, 0, 1, 0, 1, 0, 1, 0], classes  # col, row, ...
+    keys = {
+        pretty_matrix[cycle[1], cycle[0] - rows],
+        pretty_matrix[cycle[1], cycle[2] - rows],
+        pretty_matrix[cycle[3], cycle[2] - rows],
+        pretty_matrix[cycle[3], cycle[4] - rows],
+        pretty_matrix[cycle[5], cycle[4] - rows],
+        pretty_matrix[cycle[5], cycle[6] - rows],
+        pretty_matrix[cycle[7], cycle[6] - rows],
+        pretty_matrix[cycle[7], cycle[8] - rows],
+        pretty_matrix[cycle[9], cycle[8] - rows],
+        pretty_matrix[cycle[9], cycle[0] - rows],
+    }
+    print(
+        "".join(sorted(keys)),
+        "left-home" if len(set("ASDF").intersection(keys)) > 1 else "",
+        "right-home" if len(set("JKL;").intersection(keys)) > 1 else "",
+        "both-thumbs"
+        if set(thumbs[:3]).intersection(keys) and set(thumbs[3:]).intersection(keys)
+        else "",
+    )
